49061c00bc1399d06beebbf9eda563d387fb8044
claraastenz <castenz18@gmail.com>
2024-03-13 22:03:40 -0500

    [01-02] Added a ticket class -overridden the ToString() method to format the ticket information as a comma-separated string

--- Program.cs
+++ Program.cs
@@ -3,6 +3,21 @@ using System.IO;
 
 namespace TicketManagementApp
 {
+    public class Ticket
+{
+    public int TicketID { get; set; }
+    public string Summary { get; set; }
+    public string Status { get; set; }
+    public string Priority { get; set; }
+    public string Submitter { get; set; }
+    public string Assigned { get; set; }
+    public string Watching { get; set; }
+
+    public override string ToString()
+    {
+        return $"{TicketID},{Summary},{Status},{Priority},{Submitter},{Assigned},{Watching}";
+    }
+}
     class Program
     {
         static void Main(string[] args)
